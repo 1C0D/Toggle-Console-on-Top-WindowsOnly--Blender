4bcc9217a2757c3d79e09c7718730bd1ca0a6d2b
1C0D <63018905+1C0D@users.noreply.github.com>
2021-02-22 20:35:09 +0100

    2 different operators to open/show and close

--- toggle_console_on_top.py
+++ toggle_console_on_top.py
@@ -1,37 +1,7 @@
-#
-#    Copyright (c) 2017 Shane Ambler
-#
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are
-#  met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following disclaimer
-#    in the documentation and/or other materials provided with the
-#    distribution.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-
-# made in response to BSE question -
-# https://blender.stackexchange.com/q/95070/935
-
 bl_info = {
     "name": "toggle console on top",
     "author": "1C0D, HerminOs",
-    "version": (1, 0, 0),
+    "version": (1, 1, 0),
     "blender": (2, 90, 0),
     "location": "Window menu",
     "description": "Always open system console on top",
@@ -40,7 +10,8 @@ bl_info = {
 
 """
 For Windows only
-You can add a shortcut in the window menu
+-open console/show on top 
+-close console
 thanks to ErminOs
 
 """
@@ -49,34 +20,39 @@ import bpy
 import bpy
 from ctypes import windll
 
-
-def main(context):
-
-    GetConsoleWindow = windll.kernel32.GetConsoleWindow
-    ShowWindow = windll.user32.ShowWindow
-    SwitchToThisWindow = windll.user32.SwitchToThisWindow
-    IsWindowVisible = windll.user32.IsWindowVisible
-    hWnd = GetConsoleWindow()
-    if IsWindowVisible(hWnd):
-        ShowWindow(hWnd, 0) # SW_HIDE
-    else:
-        ShowWindow(hWnd, 5) # SW_SHOW
-        SwitchToThisWindow(hWnd, True) #on Top
-
-
 class TOGGLE_OT_console_on_top(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "wm.console_toggle_on_top"
     bl_label = "console toggle always on top"
+    
+    open_console : bpy.props.BoolProperty()
 
     def execute(self, context):
         
-        main(context)
+        GetConsoleWindow = windll.kernel32.GetConsoleWindow
+        ShowWindow = windll.user32.ShowWindow
+        SwitchToThisWindow = windll.user32.SwitchToThisWindow
+        IsWindowVisible = windll.user32.IsWindowVisible
+        hWnd = GetConsoleWindow()
+
+        if self.open_console:
+            if IsWindowVisible(hWnd):
+                SwitchToThisWindow(hWnd, True) #on Top
+            else:
+                ShowWindow(hWnd, 5) # SW_SHOW
+                SwitchToThisWindow(hWnd, True) #on Top
+        else:
+            if IsWindowVisible(hWnd):
+                ShowWindow(hWnd, 0) # SW_HIDE
         return {'FINISHED'}
 
 def draw(self, context):
     layout = self.layout
-    layout.operator("wm.console_toggle_on_top", text="Toggle Console On Top")
+    console_toggle = "wm.console_toggle_on_top"
+    ops = layout.operator(console_toggle, text="Console On/Top")
+    ops.open_console=True
+    ops = layout.operator(console_toggle, text="Console Off")
+    ops.open_console=False
 
 def register():
     bpy.utils.register_class(TOGGLE_OT_console_on_top)
